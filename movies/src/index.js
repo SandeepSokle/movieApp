a9b40e7129b90965e3e8e1b0e5d9514d30bc212b
Sandeep Sokle <60534028+SandeepSokle@users.noreply.github.com>
2021-08-12 10:27:27 +0530

    up

--- movies/src/index.js
+++ movies/src/index.js
@@ -1,9 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import Navbar from "./Navbar";
+import Customers from "./Customers";
+import Login from "./Login";
+import Rentals from "./Rentals";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Test from "./Test"
+
 
 ReactDOM.render(
-    <App logged={true}/>,
-  document.getElementById('root')
-);
+  <Router>
+    <Navbar />
+    <Switch>
+      <Route path="/customers">
+        <Customers />
+      </Route>
 
+      <Route path="/login">
+        <Login />
+      </Route>
+
+      <Route path="/rentals">
+        <Rentals />
+      </Route>
+
+      <Route path="/">
+        <App />
+      </Route>
+    </Switch>
+  </Router>,
+
+  document.getElementById("root")
+);
